56aacf69b469ec48cb5c12f1c07a7d60ee18d75f
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-01-17 21:24:13 -0300

    chore: configuring typescript, prisma, fastify and cors

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors)
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
+})
+
+app.listen({
+  port: 3000,
+}).then(() => {
+  console.log('Server is running on port 3000')
+})
